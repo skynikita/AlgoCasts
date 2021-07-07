8bd28204925ceade5e4e5428177eec5d527a33ff
skynikita <yangluo1105@gmail.com>
2021-07-07 17:13:00 +1000

    update solution2 pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,27 @@
 //       ' ### '
 //       '#####'
 
+function pyramid(n, row=0, level='') {
+    if(row===n){
+        return;
+    }
+    if(level.length === 2*n-1){
+        console.log(level)
+        return pyramid(n, row+1)
+    }
+    const midpoint = Math.floor((2*n -1)/2);
+    let add;
+    if(midpoint - row <= level.length && midpoint + row >= level.length){
+        add = '#'
+    }else{
+        add = ' ';
+    }
+    pyramid(n,row,level + add)
+}
+
+module.exports = pyramid;
+
+/*
 function pyramid(n) {
     const midpoint = Math.floor((2*n-1)/2);
     for (let row = 0; row < n; row++){
@@ -29,5 +50,5 @@ function pyramid(n) {
 
     }
 }
+*/
 
-module.exports = pyramid;
