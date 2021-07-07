e6cb5cfb7dfd7279567404645c90d63a73472d65
skynikita <yangluo1105@gmail.com>
2021-07-07 16:22:02 +1000

    update solution pyramid

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,20 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n, row= 0, stair = '') {
-    if(n=== row){
-        return;
-    }
-    if(n === stair.length){
+function steps(n) {
+    for (let row = 0; row < n; row++){
+        let stair = '';
+        for( let column = 0; column < n; column++ ){
+            if (column <= row){
+                stair += '#'
+            }else{
+                stair += ' ';
+            }
+        }
         console.log(stair)
-        return steps(n, row +1);
     }
-    if(stair.length <= row) {
-        stair += '#'
-    }else{
-        stair += ' ';
-    }
-    steps(n, row, stair)
 }
 
 module.exports = steps;
@@ -48,4 +46,21 @@ module.exports = steps;
         }
         console.log(stair)
     }
+}*/
+
+/*
+function steps(n, row= 0, stair = '') {
+    if(n=== row){
+        return;
+    }
+    if(n === stair.length){
+        console.log(stair)
+        return steps(n, row +1);
+    }
+    if(stair.length <= row) {
+        stair += '#'
+    }else{
+        stair += ' ';
+    }
+    steps(n, row, stair)
 }*/
