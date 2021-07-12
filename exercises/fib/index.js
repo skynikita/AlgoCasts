3cc5a231f4b06a603cf27c13c7f56dceb83027b0
skynikita <yangluo1105@gmail.com>
2021-07-12 16:06:22 +1000

    update solution2 fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,11 +9,21 @@
 //   fib(4) === 3
 
 function fib(n) {
+    if(n< 2 ){
+        return n
+    }
+    return fib(n-1) + fib(n-2)
+}
+
+module.exports = fib;
+
+/*
+function fib(n) {
     const result = [0,1];
 
     for(let i = 2; i <= n; i++){
         const a = result[i - 1];
-        const b = result[i -2];
+        const b = result[i - 2];
 
         result.push(a+b)
     }
@@ -21,6 +31,4 @@ function fib(n) {
     return result [n];
 
 
-}
-
-module.exports = fib;
+}*/
